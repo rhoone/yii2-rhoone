6d630854961c7f344a27df1c8f076b9e25cb2c2b
Zhao vistart <i@vistart.name>
2016-04-11 14:42:42 +0800

    extract validate()

--- helpers/ExtensionHelper.php
+++ helpers/ExtensionHelper.php
@@ -37,35 +37,56 @@ class ExtensionHelper
      */
     public static function add($class, $enable = false)
     {
+        $extension = static::validate($class);
+
+        $name = $extension->extensionName();
+
+        $dic = $extension->getDictionaries();
+
+        $extension = new Extension(['classname' => $extension->className(), 'name' => $name, 'enabled' => $enable == true]);
+        return $extension->save();
+    }
+
+    /**
+     * 
+     * @param string $class
+     * @return \rhoone\extension\Extension
+     * @throws InvalidParamException
+     */
+    public static function validate($class)
+    {
         if (!is_string($class)) {
             throw new InvalidParamException('the class name is not a string.');
         }
-
+        
+        if (!class_exists($class)) {
+            if (strpos($class, "\\", strlen($class) - 1) === false) {
+                $class .= "\Extension";
+            } else {
+                $class .= "Extension";
+            }
+        }
         if (!class_exists($class)) {
             throw new InvalidParamException('the class `' . $class . '` does not exist.');
         }
 
         $extension = new $class();
-
+        $class = $extension->className();
         if (!($extension instanceof \rhoone\extension\Extension)) {
             throw new InvalidParamException('the class `' . $class . '` is not an extension.');
         }
-        
-        $name = $extension->extensionName();
 
         if (Extension::find()->where(['classname' => $class])->exists()) {
             throw new InvalidParamException('the class `' . $class . '` has been added.');
         }
-        
+
         $dic = $extension->getDictionaries();
         if (empty($dic)) {
             echo "Warning: This extension does not contain any dictionaries.\n";
         } else {
             
         }
-        
-        $extension = new Extension(['classname' => $class, 'name' => $name]);
-        return $extension->save();
+        return $extension;
     }
 
     /**
