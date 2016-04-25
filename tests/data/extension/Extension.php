fa8042a6bf17316f44956257c1c870ec33580766
Zhao vistart <i@vistart.name>
2016-04-25 15:41:33 +0800

    trigger testing.

---
+++ tests/data/extension/Extension.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016 vistart
+ * @license https://vistart.name/license/
+ */
+
+namespace rhoone\tests\data\extension;
+
+/**
+ * Description of Extension
+ *
+ * @author vistart <i@vistart.name>
+ */
+class Extension extends rhoone\extension\Extension
+{
+
+    public function search($keywords)
+    {
+        
+    }
+}
